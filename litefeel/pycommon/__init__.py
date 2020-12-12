4eeb33aafebc5c6d91c57c1af5a0696fc7cc0bc4
litefeel <litefeel@gmail.com>
2020-12-13 00:39:35 +0800

    Bump version 0.4.2

--- litefeel/pycommon/__init__.py
+++ litefeel/pycommon/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 
 'version'
-__version__ = '0.4.1'
+__version__ = '0.4.2'
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="litefeel-pycommon",
-    version="0.4.1",
+    version="0.4.2",
     description="my python common lib",
     author="litefeel",
     author_email="litefeel@gmail.com",
