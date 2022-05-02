554f3dab1de7a05a801e832b12887465419425fe
litefeel <litefeel@gmail.com>
2022-05-02 17:50:25 +0800

    Update data.py

--- litefeel/pycommon/data.py
+++ litefeel/pycommon/data.py
@@ -2,16 +2,20 @@ import openpyxl
 import csv
 
 
-def save_csv(filename, rows: list[list]):
+def save_csv(filename, rows: list[list], header: list = None):
     with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
+        if header:
+            writer.writerow(header)
         for row in rows:
             writer.writerow(row)
 
 
-def save_xlsx(filename, rows: list[list]):
+def save_xlsx(filename, rows: list[list], header: list = None):
     wb = openpyxl.Workbook()
     ws = wb.active
+    if header:
+        ws.append(header)
     for row in rows:
         ws.append(row)
     wb.save(filename)
