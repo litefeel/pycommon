7a6caa9894ada9de31fb9c65a8fbbf39abc94306
litefeel <litefeel@gmail.com>
2022-05-28 15:16:15 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="litefeel-pycommon",
-    version="0.4.11",
+    version="0.4.12",
     description="my python common lib",
     author="litefeel",
     author_email="litefeel@gmail.com",
